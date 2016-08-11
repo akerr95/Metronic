2738df0a4719f305f269a30884913ad36803c2bc
akerr95 <akerr_95@hotmail.com>
2016-08-11 00:52:17 -0500

    Updated the Navigation Component to more represent the react philosophy

--- public/Components/Metro.js
+++ public/Components/Metro.js
@@ -1,27 +1,64 @@
 /**
  * Created by akerr on 8/6/16.
  */
-var React = require ("react");
-class UI extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {name: props.name};
-  }
-  render(){
-    return (<div>
-      <p>Hello {this.state.name}</p>
-    </div>);
-  }
-}
+var React = require("react");
 
-class Header extends React.Component{
-  render(){
-    return (<div>
-      <p>Hello {this.state.name}</p>
-    </div>);
-  }
+class Navigation extends React.Component {
+    render() {
+        return (<div>
+            <Identity data= {this.props.data}/>
+            <Menu/>
+            <Main_Menu text = {this.props.data.text}/>
+        </div>);
+    }
 }
 
 
+class Identity extends React.Component {
+    render() {
+        return (
+            <a href={this.props.data.index}>
+                <img src={this.props.data.logo_Img} alt={this.props.data.alt}/>
+            </a>
+        );
+    }
+}
+class Menu extends React.Component {
+    render() {
+        return (
+            <a href="#">
+                <p>Vision</p>
+            </a>
+        );
+    }
+}
+class Main_Menu extends React.Component {
+    render() {
+        return (
+            <ul>
+                <Icon text={this.props.text}></Icon>
+            </ul>
+        );
+    }
+}
 
-module.exports =Header;
+class Icon extends React.Component {
+    render() {
+        return (
+            <li>{this.props.text}</li>
+        );
+    }
+}
+
+
+Navigation.propTypes = {
+    name: React.PropTypes.string
+};
+Navigation.defaultProps = {
+    name: "Alec"
+};
+
+
+module.exports = {
+    Navigation
+};
