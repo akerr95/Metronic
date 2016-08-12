d63f53edbb35942b11dc8538cddee857b26c8e93
Alec Kerr <akerr_95@hotmail.com>
2016-08-12 13:49:22 -0500

    Updated navigation data structure to allow for breaking up the data into groups.

--- public/Components/main.js
+++ public/Components/main.js
@@ -5,9 +5,16 @@ var UI = require('./Metro.js');
 var React = require('react');
 var ReactDom = require('react-dom');
 var data = {
-    index:'#',
-    logo_Img: "https://d13yacurqjgara.cloudfront.net/users/109605/screenshots/2891300/bourbon_1x.png",
-    alt: "Image of Bourbon",
-    text:"Random text"
-}
-ReactDom.render(<UI.Navigation data={data}/>, document.getElementById('example'));
+    identityInfo: {
+        index: '#',
+        logo_Img: "https://d13yacurqjgara.cloudfront.net/users/109605/screenshots/2891300/bourbon_1x.png",
+        alt: "Image of Bourbon",
+    },
+    mainMenuInfo: {
+        icons: [{name: 'Home', iconImg: 'example',notify: 7},
+            {name: 'Settings', iconImg: 'example',notify: 7},
+            {name: 'Profile', iconImg: 'example',notify: 7}
+        ]
+    }
+};
+ReactDom.render(<UI.Navigation info={data}/>, document.getElementById('example'));
